e6de2adaef0021775e349ddef6539ae73e163e3e
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2021-06-28 21:18:57 +0100

    Fixed bug

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from nogeometric import HGNN
 
 x = torch.ones(17)
 x = torch.diag(x)
+# x = torch.rand((500,5))
 
-hyperedge_index = {2:torch.tensor([[0, 1, 3, 4, 6, 7],
+hyperedge_index = { 1:torch.tensor([[0, 1, 2, 3, 4, 5],
+                                [5, 6, 7, 8, 9, 10]], dtype=torch.long),
+                    2:torch.tensor([[0, 1, 3, 4, 6, 7],
                                 [2, 2, 5, 5, 8, 8]], dtype=torch.long),
-                   3:torch.tensor([[9, 10, 11, 13, 14, 15],
+                    3:torch.tensor([[9, 10, 11, 13, 14, 15],
                                 [12, 12, 12, 16, 16, 16]], dtype=torch.long)}
 
 model = HGNN()
--- nogeometric.py
+++ nogeometric.py
@@ -8,21 +8,26 @@ class HGNN(nn.Module):
         super(HGNN, self).__init__()
 
     def forward(self,  x, hyperedge_index):
-        print('Message')
-        index = torch.tensor([],dtype=int)
+        index = torch.tensor([],dtype=torch.int16)
         x_j = torch.tensor([])
+        # Loop through hyperedges with i src nodes -- second loop for different relation types is required
         for i in hyperedge_index:
             ind = torch.reshape(hyperedge_index[i][1], (-1, i))
             ind = ind[:, 0]
             index = torch.cat((index, ind), dim=0)
             help = x[hyperedge_index[i][0]]
             s = help.size()[1] * i
+            # Concat feature vectors of src nodes for hyperedges
             concat = torch.reshape(help, (-1, s))
+            # Dummy weight vectors -- message function
             weight = torch.diag(torch.ones(x.size()[1])).repeat(i, 1)
             transformed = torch.mm(concat, weight)
+            # Tensor with all messages
             x_j = torch.cat((x_j, transformed))
         index = index.unsqueeze(1).expand_as(x_j)
-        out = scatter.scatter_add(src=x_j, index=index, dim=0)
+        # Aggregate function
+        out = scatter.scatter_add(src=x_j, index=index, out=torch.zeros_like(x), dim=0)
+        # Combine function
         out = x + out
         return out
 
