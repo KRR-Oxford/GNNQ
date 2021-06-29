9740190c7f5d37772baca2fe9d79672576205a79
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2021-06-29 16:04:09 +0100

    First semi working version with edge types

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nogeometric import HGNN
 
 x = torch.ones(17)
 x = torch.diag(x)
-# x = torch.rand((500,5))
+# x = torch.rand((20,5))
 
 hyperedge_index = { 1:torch.tensor([[0, 1, 2, 3, 4, 5],
                                 [5, 6, 7, 8, 9, 10]], dtype=torch.long),
@@ -12,6 +12,10 @@ hyperedge_index = { 1:torch.tensor([[0, 1, 2, 3, 4, 5],
                     3:torch.tensor([[9, 10, 11, 13, 14, 15],
                                 [12, 12, 12, 16, 16, 16]], dtype=torch.long)}
 
-model = HGNN()
-out = model(x, hyperedge_index)
+hyperedge_type = {1:torch.tensor([1,1,1,1,2,2]),2:torch.tensor([0,1,2]), 3:torch.tensor([0,1])}
+
+num_edge_types_by_shape = {1:3, 2:3, 3:3}
+
+model = HGNN(17, num_edge_types_by_shape)
+out = model(x, hyperedge_index, hyperedge_type)
 print(out)
--- nogeometric.py
+++ nogeometric.py
@@ -4,26 +4,41 @@ import torch_scatter as scatter
 
 
 class HGNN(nn.Module):
-    def __init__(self):
+    def __init__(self, base_dim, num_edge_types_by_shape):
         super(HGNN, self).__init__()
+        self.base_dim = base_dim
+        self.num_edge_types_by_shape = num_edge_types_by_shape
+        self.weights = {}
+        # Shape is the number of src nodes -- we consider edges of the form ((u_0,...,u_{n-1}), R, v)
+        for shape in num_edge_types_by_shape:
+            self.weights[shape] = torch.nn.Parameter(torch.zeros(num_edge_types_by_shape[shape], base_dim * shape, base_dim))
+            # Dummy weights for debugging
+            weight = torch.diag(torch.ones(base_dim)).repeat(shape, 1).repeat(num_edge_types_by_shape[shape],1,1)
+            for i in range(1, num_edge_types_by_shape[shape] + 1):
+                weight[i-1] = weight[i-1] * i
+            self.weights[shape] = weight
 
-    def forward(self,  x, hyperedge_index):
+    def forward(self,  x, hyperedge_index, hyperedge_type):
         index = torch.tensor([],dtype=torch.int16)
         x_j = torch.tensor([])
-        # Loop through hyperedges with i src nodes -- second loop for different relation types is required
-        for i in hyperedge_index:
-            ind = torch.reshape(hyperedge_index[i][1], (-1, i))
-            ind = ind[:, 0]
-            index = torch.cat((index, ind), dim=0)
-            help = x[hyperedge_index[i][0]]
-            s = help.size()[1] * i
-            # Concat feature vectors of src nodes for hyperedges
-            concat = torch.reshape(help, (-1, s))
-            # Dummy weight vectors -- message function
-            weight = torch.diag(torch.ones(x.size()[1])).repeat(i, 1)
-            transformed = torch.mm(concat, weight)
-            # Tensor with all messages
-            x_j = torch.cat((x_j, transformed))
+        # Loop through hyperedges with different shapes (num of src nodes)
+        for shape in hyperedge_index:
+            # Loop through edge_types for every shape
+            for edge_type in range(0, self.num_edge_types_by_shape[shape]):
+                mask = (hyperedge_type[shape] == edge_type)
+                mask = mask.unsqueeze(1).repeat(1,shape).flatten()
+                ind = torch.reshape(hyperedge_index[shape][1][mask], (-1, shape))
+                if (ind.nelement() != 0):
+                    ind = ind[:, 0]
+                    index = torch.cat((index, ind), dim=0)
+                    tmp = x[hyperedge_index[shape][0][mask]]
+                    s = tmp.size()[1] * shape
+                    # Concat feature vectors of src nodes for hyperedges
+                    concat = torch.reshape(tmp, (-1, s))
+                    weight = self.weights[shape][edge_type]
+                    transformed = torch.mm(concat, weight)
+                    # Tensor with all messages
+                    x_j = torch.cat((x_j, transformed))
         index = index.unsqueeze(1).expand_as(x_j)
         # Aggregate function
         out = scatter.scatter_add(src=x_j, index=index, out=torch.zeros_like(x), dim=0)
