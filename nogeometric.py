3dabad72e6a1c279925820ad4b324225a21a350d
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2021-06-30 18:11:02 +0100

    Fixed bug that let to some parameters not beeing registered and implemented dummy training loop

--- nogeometric.py
+++ nogeometric.py
@@ -9,21 +9,24 @@ class HGNNLayer(nn.Module):
         super(HGNNLayer, self).__init__()
         self.base_dim = base_dim
         self.num_edge_types_by_shape = num_edge_types_by_shape
-        self.lin = {}
+        # Includes bias
+        self.C = torch.nn.Linear(base_dim, base_dim)
+        self.A = torch.nn.ParameterDict({})
         # Shape is the number of src nodes -- we consider edges of the form ((u_0,...,u_{n-1}), R, v)
         for shape in num_edge_types_by_shape:
-            self.lin[shape] = torch.nn.Parameter(
+            self.A[str(shape)] = torch.nn.Parameter(
                 torch.zeros(num_edge_types_by_shape[shape], base_dim * shape, base_dim))
-            nn.init.xavier_normal(self.lin[shape])
+            nn.init.xavier_normal_(self.A[str(shape)])
             # Dummy weights for debugging
-            weight = torch.diag(torch.ones(base_dim)).repeat(shape, 1).repeat(num_edge_types_by_shape[shape], 1, 1)
-            for i in range(1, num_edge_types_by_shape[shape] + 1):
-                weight[i - 1] = weight[i - 1] * i
-            self.lin[shape] = weight
+            # weight = torch.diag(torch.ones(base_dim)).repeat(shape, 1).repeat(num_edge_types_by_shape[shape], 1, 1)
+            # for i in range(1, num_edge_types_by_shape[shape] + 1):
+            #     weight[i - 1] = weight[i - 1] * i
+            # self.A[shape] = weight
 
+    # h = x_iC + b + SUM_shape SUM_type SUM_neighbours x_jA_shape,type
     def forward(self, x, hyperedge_index, hyperedge_type):
         index = torch.tensor([], dtype=torch.int16)
-        x_j = torch.tensor([])
+        x_j = torch.tensor([], dtype=torch.float16)
         # Loop through hyperedges with different shapes (num of src nodes)
         for shape in hyperedge_index:
             # Loop through edge_types for every shape
@@ -39,35 +42,43 @@ class HGNNLayer(nn.Module):
                     tmp = x[hyperedge_index[shape][0][mask]]
                     s = tmp.size()[1] * shape
                     # Concat feature vectors of src nodes for hyperedges
-                    concatted = torch.reshape(tmp, (-1, s))
-                    weights = self.lin[shape][edge_type]
-                    transformed = torch.mm(concatted, weights)
+                    tmp = torch.reshape(tmp, (-1, s))
+                    weights = self.A[str(shape)][edge_type]
+                    tmp = tmp.mm(weights)
                     # Normalisation
                     _, revers_i, counts = torch.unique(i, return_inverse=True, return_counts=True)
                     norm = 1 / counts[revers_i]
-                    transformed = norm.unsqueeze(1) * transformed
+                    tmp = norm.unsqueeze(1) * tmp
                     # Tensor with all messages
-                    x_j = torch.cat((x_j, transformed))
+                    x_j = torch.cat((x_j, tmp))
         index = index.unsqueeze(1).expand_as(x_j)
-        # Aggregate function
-        out = scatter.scatter_add(src=x_j, index=index, out=torch.zeros_like(x), dim=0)
-        # Combine function
-        out = x + out
-        return out
+        # Aggregate
+        agg = scatter.scatter_add(src=x_j, index=index, out=torch.zeros_like(x), dim=0)
+        # Combine
+        h = self.C(x) + agg
+        return h
 
 
 class HGNN(nn.Module):
     def __init__(self, base_dim, num_edge_types_by_shape):
         super(HGNN, self).__init__()
         self.num_edge_types_by_shape = num_edge_types_by_shape
-        self.layer1 = HGNNLayer(base_dim, num_edge_types_by_shape)
-        self.activation1 = f.relu
-        self.layer2 = HGNNLayer(base_dim, num_edge_types_by_shape)
-        self.activation2 = f.relu
+        self.msg_layer_1 = HGNNLayer(base_dim, num_edge_types_by_shape)
+        self.activation1 = torch.relu
+        self.msg_layer_2 = HGNNLayer(base_dim, num_edge_types_by_shape)
+        self.activation2 = torch.relu
+        self.lin_layer_1 = nn.Linear(base_dim, base_dim)
+        self.activation3 = torch.relu
+        self.lin_layer_2 = nn.Linear(base_dim, 1)
+        self.activation4 = torch.sigmoid
 
     def forward(self, x, hyperedge_index, hyperedge_type):
-        h = self.layer1(x, hyperedge_index, hyperedge_type)
+        h = self.msg_layer_1(x, hyperedge_index, hyperedge_type)
         h = self.activation1(h)
-        h = self.layer2(h, hyperedge_index, hyperedge_type)
+        h = self.msg_layer_2(h, hyperedge_index, hyperedge_type)
         h = self.activation2(h)
+        h = self.lin_layer_1(h)
+        h = self.activation3(h)
+        h = self.lin_layer_2(h)
+        h = self.activation4(h)
         return h
