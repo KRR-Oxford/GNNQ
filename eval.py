62e7d35e6905ab1900e4f48c87fcca06122d4d09
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2021-07-30 17:59:37 +0200

    Save before weekend

--- eval.py
+++ eval.py
@@ -1,34 +1,25 @@
 import torch
 from model import HGNN
-from data import data
+from data import load_data
 import numpy as np
+import argparse
 from torchmetrics import Accuracy, Precision, Recall
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-triples, entity2id, relation2id, id2entity, id2relation = data(['train/graph.ttl'])
-with open('val/answers_val_small.npy', 'rb') as f:
-    true_answers = torch.tensor(np.load(f))
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--train_data', type=str, default='train')
+parser.add_argument('--val_data', type=str, default='val')
+parser.add_argument('--base_dim', type=int, default=16)
+parser.add_argument('--num_layers', type=int, default=3)
+parser.add_argument('--epochs', type=int, default=20)
+parser.add_argument('--val_epochs', type=int, default=10)
+parser.add_argument('--lr', type=int, default=0.1)
+args = parser.parse_args()
 
-num_nodes = len(entity2id)
-base_dim = 16
-num_layers = 3
+x_val, y_val, hyperedge_index_val, hyperedge_type_val, num_edge_types_by_shape , _= load_data(args.val_data + '/graph.ttl', args.val_data + '/answers.npy', args.val_data + '/sub_query_answers_val.npy', args.base_dim)
 
-x = torch.cat((torch.ones(num_nodes,1), torch.zeros(num_nodes,base_dim - 1)), dim=1)
-
-# K - hot vector indicating all answers
-y = torch.scatter(torch.zeros(num_nodes, dtype=torch.int16), 0, true_answers, torch.ones(num_nodes,dtype=torch.int16))
-
-entities = torch.tensor(triples['train/graph.ttl']).t()[[0,2]]
-entities = torch.cat((entities, entities[[1,0]]), dim=1)
-relations = torch.tensor(triples['train/graph.ttl']).t()[1]
-relations = torch.cat((relations, relations + torch.max(relations) + 1),dim=0)
-hyperedge_index = {1: entities}
-hyperedge_type = {1: relations}
-
-num_edge_types_by_shape = {1:len(relation2id) * 2}
-
-model = HGNN(base_dim, num_edge_types_by_shape, num_layers)
+model = HGNN(args.base_dim, num_edge_types_by_shape, args.num_layers)
 model.to(device)
 help = model.parameters()
 for param in model.parameters():
@@ -37,15 +28,13 @@ for param in model.parameters():
 model.load_state_dict(torch.load('./model.pt'))
 
 model.eval()
-pred = model(x, hyperedge_index, hyperedge_type).flatten()
-print(pred[true_answers])
-print(pred[torch.randint(x.size()[0], (len(true_answers),))])
+pred = model(x_val, hyperedge_index_val, hyperedge_type_val).flatten()
 accuracy = Accuracy(threshold=0.5)
 precision = Precision(threshold=0.5)
 recall = Recall(threshold=0.5)
-acc = accuracy(pred,y)
-pre = precision(pred,y)
-rec = recall(pred,y)
+acc = accuracy(pred,y_val)
+pre = precision(pred,y_val)
+rec = recall(pred,y_val)
 
 
 print('Accuracy ' + str(acc.item()))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 from model import HGNN
-from data import data
+from data import load_data
 import numpy as np
 import argparse
 from torchmetrics import Accuracy, Precision, Recall
@@ -22,33 +22,6 @@ learning_rate = args.lr
 val_epochs = args.val_epochs
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-def load_data(dataset_path, answers_path, subquery_answers_path, base_dim):
-    triples, entity2id, relation2id, id2entity, id2relation = data([dataset_path])
-    with open(answers_path, 'rb') as f:
-        answer_indices = torch.tensor(np.load(f))
-    num_nodes = len(entity2id)
-    x = torch.cat((torch.ones(num_nodes,1), torch.zeros(num_nodes,base_dim - 1)), dim=1)
-
-    # K - hot vector indicating all answers
-    y = torch.scatter(torch.zeros(num_nodes, dtype=torch.int16), 0, answer_indices, torch.ones(num_nodes, dtype=torch.int16))
-
-    edges = torch.tensor(triples[dataset_path]).t()[[0,2]]
-    edges = torch.cat((edges, edges[[1,0]]), dim=1)
-    edge_type = torch.tensor(triples[dataset_path]).t()[1]
-    edge_type = torch.cat((edge_type, edge_type + torch.max(edge_type) + 1),dim=0)
-    hyperedge_index = {1: edges}
-    hyperedge_type = {1: edge_type}
-    with open(subquery_answers_path, 'rb') as f:
-        subquery_answer_indices = torch.tensor(np.load(f))
-        hyper = torch.tensor([], dtype=torch.int16)
-        for hyperedge_indices in subquery_answer_indices:
-            hyper = torch.cat((hyper, torch.stack((torch.tensor(hyperedge_indices[1:]),torch.tensor(hyperedge_indices[0]).repeat(len(hyperedge_indices)-1)),dim=0)),dim=1)
-        types = torch.zeros([len(subquery_answer_indices)], dtype=torch.int16)
-    hyperedge_index[5] = hyper
-    hyperedge_type[5] = types
-    num_edge_types_by_shape = {1: len(relation2id) * 2, 5:1}
-    return x, y, hyperedge_index, hyperedge_type, num_edge_types_by_shape, answer_indices
 
 x_train, y_train, hyperedge_index_train, hyperedge_type_train, num_edge_types_by_shape_train, answer_indices = load_data(args.train_data + '/graph.ttl', args.train_data + '/answers.npy', args.train_data + '/sub_query_answers.npy', base_dim)
 x_val, y_val, hyperedge_index_val, hyperedge_type_val, _ , _= load_data(args.val_data + '/graph.ttl', args.val_data + '/answers.npy', args.val_data + '/sub_query_answers_val.npy', base_dim)
