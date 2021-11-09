80b1b4564fb8372d82aa81f464b6a2b196a9b57d
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2021-11-09 13:21:11 +0000

    Added test to verify output of subqueries

--- unit_testing.py
+++ unit_testing.py
@@ -2,8 +2,7 @@ import unittest
 import torch
 from model import HGNNLayer
 
-
-class MyTestCase(unittest.TestCase):
+class ModelTestCase(unittest.TestCase):
 
     def test_message_passing(self):
         base_dim = 16
@@ -64,6 +63,30 @@ class MyTestCase(unittest.TestCase):
          0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.0000, 0.0000],
         [0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
          0.0000, 0.0000, 1.0000, 0.0000, 1.0000, 1.0000, 1.0000]])))
+
+from data_utils import create_indices_dict, compute_subquery_answers
+from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
+
+class QueryTestCase(unittest.TestCase):
+
+    def test_subquery_answers(self):
+         g = Graph()
+         g.parse('datasets/dummy/corrupted_graph.nt', format="nt")
+         _, entity2id, _ = create_indices_dict(g)
+         subquery = prepareQuery('SELECT ?v0 ?v4 ?v2 ?v5 WHERE { ?v0  <http://schema.org/text> ?v2 . ?v0  <http://purl.org/stuff/rev#hasReview> ?v4 . ?v4 <http://purl.org/stuff/rev#title> ?v5 }')
+         key = str([str(var) for var in subquery.algebra['PV']])
+         qres = g.query(subquery)
+         answers = []
+         for row in qres:
+             answers.append([entity2id[str(entity).strip()] for entity in row])
+         answers = torch.tensor(answers)
+         answers = torch.stack((answers[:, 1:].flatten(), answers[:, 0].unsqueeze(1).repeat((1, answers.size()[1] - 1)).flatten()), dim=0)
+         query = 'SELECT distinct ?v0 WHERE { ?v0  <http://schema.org/caption> ?v1 . ?v0   <http://schema.org/text> ?v2 . ?v0 <http://schema.org/contentRating> ?v3 . ?v0   <http://purl.org/stuff/rev#hasReview> ?v4 .  ?v4 <http://purl.org/stuff/rev#title> ?v5 . ?v4  <http://purl.org/stuff/rev#reviewer> ?v6 . ?v7 <http://schema.org/actor> ?v6 . ?v7 <http://schema.org/language> ?v8  }'
+         subquery_answers, _ = compute_subquery_answers(g, query, 'not greedy', 2, 6, entity2id)
+         answers2 = subquery_answers[key]
+         self.assertTrue(torch.equal(answers, answers2))
+
 
 
 if __name__ == '__main__':
