4d1ab18d591b5e3ebc7e34db255147c27edfb3b5
MaxPflueger <maximilian.pflueger@chch.ox.ac.uk>
2022-07-27 00:48:20 +0100

    Fixed bug occuring when running eval.py

--- eval.py
+++ eval.py
@@ -87,12 +87,13 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_directory', type=str)
+    parser.add_argument('--model_file', type=str)
+    parser.add_argument('--config_file', type=str)
     parser.add_argument('--test_data', type=str, nargs='+')
     args = parser.parse_args()
 
-    with open(os.path.join(args.log_directory, 'config.txt'), 'r') as f:
+    with open(args.config_file, 'r') as f:
         run_args = json.load(f)
 
-    eval(test_data=args.test_data, model_directory=args.model_directory,
+    eval(test_data=args.test_data, model_directory=args.model_file,
          aug=run_args['aug'], device=device)
